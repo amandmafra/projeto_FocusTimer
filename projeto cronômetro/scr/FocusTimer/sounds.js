e8333438ae528907b850e358fb7d78e694422578
amandmafra <135349875+amandmafra@users.noreply.github.com>
2024-03-04 09:53:29 -0300

    Add files via upload

---
+++ projeto cronômetro/scr/FocusTimer/sounds.js
@@ -0,0 +1,7 @@
+export const buttonPressAudio = new Audio('./image/button-press.wav')
+
+export const kichenTimer = new Audio('./image/kichen-timer.mp3')
+
+export const bgAudio = new Audio('./image/mp3-Audio.mp3')
+
+bgAudio.loop = true
